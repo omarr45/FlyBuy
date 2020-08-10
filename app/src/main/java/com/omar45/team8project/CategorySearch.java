554ccb62d0ae61ceefa9cb0f7e4972f0d2235c28
omarr45 <58887202+omarr45@users.noreply.github.com>
2020-08-10 22:32:39 +0200

    Added The remaining activity and edited the Manifest

--- app/src/main/java/com/omar45/team8project/CategorySearch.java
+++ app/src/main/java/com/omar45/team8project/CategorySearch.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class UserAccount extends AppCompatActivity {
+public class CategorySearch extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_account);
+        setContentView(R.layout.activity_category_search);
     }
 }

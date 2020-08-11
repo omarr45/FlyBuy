8b965e9ad0972f58bbc514767785a38dfc94c08f
omarr45 <58887202+omarr45@users.noreply.github.com>
2020-08-11 02:20:15 +0200

    Added The design for Login Page

--- app/src/main/java/com/omar45/team8project/SignInActivity.java
+++ app/src/main/java/com/omar45/team8project/SignInActivity.java
@@ -3,12 +3,17 @@ package com.omar45.team8project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class SignInActivity extends AppCompatActivity {
+
+    EditText edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        edit = findViewById(R.id.email);
     }
 }

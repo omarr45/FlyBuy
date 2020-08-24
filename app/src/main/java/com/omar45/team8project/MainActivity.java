c9329dfb8f075e07d48af2dc90a2a54788fec18a
omarr45 <58887202+omarr45@users.noreply.github.com>
2020-08-24 15:38:42 +0200

    finished activity_main.xml

--- app/src/main/java/com/omar45/team8project/MainActivity.java
+++ app/src/main/java/com/omar45/team8project/MainActivity.java
@@ -4,11 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    ImageSlider imageSlider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageSlider = findViewById(R.id.imageSliderMain);
+        List<SlideModel> slideModels = new ArrayList<SlideModel>();
+        slideModels.add(new SlideModel(R.drawable.banner2, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.banner3, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.banner1, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.banner4, ScaleTypes.FIT));
+        imageSlider.startSliding(2000);
+        imageSlider.setImageList(slideModels,ScaleTypes.FIT);
+
     }
 }

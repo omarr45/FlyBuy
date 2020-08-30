29c1672b6ad98079b3edc16202f933bc2810032c
omarr45 <58887202+omarr45@users.noreply.github.com>
2020-08-31 00:41:04 +0200

    updated cart

--- app/src/main/java/com/omar45/team8project/ShoppingCart.java
+++ app/src/main/java/com/omar45/team8project/ShoppingCart.java
@@ -1,7 +1,6 @@
 package com.omar45.team8project;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -12,12 +11,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.widget.Toolbar;
-
-import com.facebook.appevents.suggestedevents.ViewOnClickListener;
 
 import io.reactivex.CompletableObserver;
 import io.reactivex.SingleObserver;
@@ -25,38 +21,22 @@ import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.security.auth.login.LoginException;
-
 public class ShoppingCart extends AppCompatActivity {
+
     Toolbar toolbar;
-  //  CartDatabase cartDatabase;
-   public static ProductDatabase productDatabase;
-   private CartAdapter adapter;
+    public static ProductDatabase productDatabase;
+    private CartAdapter adapter;
     Button checkout;
-    Context context;
-    public static List<Product>productList;
-    public List<Cart>pCart;
     private RecyclerView recyclerView;
-    public static float itemPrice,totalPrice;
-    TextView total;
+    public float totalPrice;
+    TextView _total;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_cart);
-
-         productList = new ArrayList<>();
-
-        //cartDatabase=CartDatabase.getInstance(this);
-        productDatabase=ProductDatabase.getInstance(this);
-        checkout = findViewById(R.id.checkout_button);
-
-
-          total=findViewById(R.id.total);
-          total.setText((int) totalPrice);
-        //  productList
 
         toolbar = findViewById(R.id.toolbarCart);
         setSupportActionBar(toolbar);
@@ -66,11 +46,20 @@ public class ShoppingCart extends AppCompatActivity {
             getSupportActionBar().setTitle("Cart");
         }
 
+        _total       = findViewById(R.id.total);
+        checkout     = findViewById(R.id.checkout_button);
+        recyclerView = findViewById(R.id.shopping_recycler_view);
+
+        totalPrice  = (float) 0.0;
+
+        adapter     = new CartAdapter(this);
+
+        productDatabase=ProductDatabase.getInstance(this);
+
         Intent in = getIntent();
         final int id = in.getIntExtra("id", 50);
         final int quantity=in.getIntExtra("quantity",1);
-        recyclerView = findViewById(R.id.shopping_recycler_view);
-        adapter = new CartAdapter(this);
+
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
 
@@ -89,10 +78,9 @@ public class ShoppingCart extends AppCompatActivity {
 
                         @Override
                         public void onSuccess(List<Product> products) {
-                            productList.add(products.get(0));
+                            Product p = products.get(0);
 
-                            c_database.cartDao().addToCart(new Cart(productList.get(0).getId(), productList.get(0).getName()
-                                    , (productList.get(0).getPrice()*quantity), productList.get(0).getImg1(), quantity))
+                            c_database.cartDao().addToCart(new Cart(id, p.getName(), p.getPrice(), p.getImg1(), quantity))
                                     .subscribeOn(Schedulers.computation())
                                     .subscribe(new CompletableObserver() {
                                         @Override
@@ -101,18 +89,12 @@ public class ShoppingCart extends AppCompatActivity {
 
                                         @Override
                                         public void onComplete() {
-                                            itemPrice+=(productList.get(0).getPrice()*quantity);
-
                                         }
 
                                         @Override
                                         public void onError(Throwable e) {
                                         }
                                     });
-
-                            //adapter.addToCart(productList);
-                            adapter.notifyDataSetChanged();
-
                         }
 
                         @Override
@@ -123,8 +105,8 @@ public class ShoppingCart extends AppCompatActivity {
         }
 
         c_database.cartDao().getCartItems()
-                .subscribeOn(Schedulers.computation())
                 .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.computation())
                 .subscribe(new SingleObserver<List<Cart>>() {
                     @Override
                     public void onSubscribe(Disposable d) {
@@ -132,11 +114,17 @@ public class ShoppingCart extends AppCompatActivity {
 
                     @Override
                     public void onSuccess(List<Cart> cartList) {
-                        for(int i=0; i<cartList.size();i++) {
-                            totalPrice += (cartList.get(i).getCart_item_price());
+
+                        adapter.setList(cartList);
+                        adapter.notifyDataSetChanged();
+                        recyclerView.setAdapter(adapter);
+
+                        for(int i=0; i<cartList.size(); i++) {
+                            totalPrice += (1.0 * cartList.get(i).getCart_item_price() * quantity);
                             Log.i("total",String.valueOf(totalPrice));
                         }
-                        adapter.setList(cartList);
+
+                        _total.setText(totalPrice + " EGP");
                     }
 
                     @Override
@@ -149,10 +137,10 @@ public class ShoppingCart extends AppCompatActivity {
                 @Override
                 public void onClick(View view) {
                     startActivity(new Intent(ShoppingCart.this, ConfirmationOrder.class));
-
                 }
             });
         }
+
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
